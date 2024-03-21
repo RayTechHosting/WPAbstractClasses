a7d9dbb001365781e7a06f0aa3677496e4967c9b
Reaper1988 <royk@myraytech.net>
2024-03-20 21:33:55 -0300

    Meta value init wrong place

--- src/Utilities/Query.php
+++ src/Utilities/Query.php
@@ -13,8 +13,8 @@ class Query {
 
 	protected function pick_fields($id, $posts, $list) {
 		$array = [];
-		$meta  = '';
 		foreach ($posts as $post) {
+			$meta  = '';
 			if(is_int($post)) {
 				if(isset($list['meta']) ) {
 					foreach ($list['meta'] as $meta_key) {
@@ -28,11 +28,12 @@ class Query {
 			} else {
 				if(isset($list['meta']) ) {
 					foreach ($list['meta'] as $meta_key) {
-						$meta_data = get_post_meta($post, $meta_key, true) . ' ';
+						$meta_data = get_post_meta($post->ID, $meta_key, true) . ' ';
 						if(is_string($meta_data) ) {
 							$meta .= $meta_data;
 						}
 					}
+					$array[$post->ID] = rtrim($meta, ' ');
 				}
 				if(isset($list['field']) ) { 
 					$field = $list['field'];
